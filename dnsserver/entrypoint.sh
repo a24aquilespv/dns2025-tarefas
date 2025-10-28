0c3e1c9b08c1349b60684095fde21a9d7bc41c12
a24aquilespv <a24aquilespv@iessanclemente.net>
2025-10-28 10:27:26 +0100

    Configurar logs

--- dnsserver/entrypoint.sh
+++ dnsserver/entrypoint.sh
@@ -2,4 +2,13 @@
 
 
 #chmod a+x safsdadfs
+
+# bind tiene que trabajar sobre la carpeta logs dentro
+# del contenedor
+chmod 770 /logs
+chmod 660 /logs/*
+chown root:bind -R /logs
+
+# Asignarle el grupo propietario "bind" a /logs
+rsyslogd
 named -u bind "$@"
